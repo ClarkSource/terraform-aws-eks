2c78b2abdce7f112605383bbea2b4ee1ad9b9871
RamezWasfy <ramez.asshak@clark.de>
2019-12-10 14:01:48 +0100

    [CLOUD-653] Added description to security groups for EKS workers

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -38,11 +38,13 @@ resource "random_string" "suffix" {
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   vpc_id      = module.vpc.vpc_id
+  description = "EKS Worker Group 1 Security Group"
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    description = "Inbound allow SSH access to EKS worker group 1 from CIDR 10.0.0.0/8"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
 
     cidr_blocks = [
       "10.0.0.0/8",
@@ -53,11 +55,13 @@ resource "aws_security_group" "worker_group_mgmt_one" {
 resource "aws_security_group" "worker_group_mgmt_two" {
   name_prefix = "worker_group_mgmt_two"
   vpc_id      = module.vpc.vpc_id
+  description = "EKS Worker Group 2 Security Group"
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    description = "Inbound allow SSH access to EKS worker group 2 from CIDR 192.168.0.0/16"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
 
     cidr_blocks = [
       "192.168.0.0/16",
@@ -68,11 +72,13 @@ resource "aws_security_group" "worker_group_mgmt_two" {
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
   vpc_id      = module.vpc.vpc_id
+  description = "EKS Worker Groups Security Group"
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    description = "Inbound allow SSH access to EKS worker groups from CIDR 10.0.0.0/8 ,172.16.0.0/12 ,192.168.0.0/16"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
 
     cidr_blocks = [
       "10.0.0.0/8",
